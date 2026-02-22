87b7a03ce98f142718197a8c33912673b762f2cf
tingoman <104537986+tingoman@users.noreply.github.com>
2026-02-21 16:32:42 -0800

    Create table.lua

---
+++ table.lua
@@ -0,0 +1,24 @@
+local amethyst = {
+    ['Aimbot'] = {
+        ['Enabled']       = true,
+        ['FOV']           = 120,
+        ['PreciseMouse']  = false,
+
+        ['Easing']        = 'Circular',
+        ['Smoothness']    = 0.5,
+    },
+
+    ['Triggerbot'] = {
+        ['Enabled']       = true,
+        ['RequireTool']   = true,
+
+        ['HoldTime']      = 0,
+        ['Cooldown']      = 0
+    },
+
+    ['Checks'] = {
+        ['IgnoreDead']      = true,
+        ['CheckTeam']       = true,
+        ['CheckForceField'] = true,
+    }
+}
